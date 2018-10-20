5a4bfe9819503e08e5bea5ea3b749052ae47ebcc
jovace <jorge.vazquez.acevedo@alumnos.upm.es>
2018-10-20 12:21:41 +0200

    Initial commit

---
+++ src/main/java/com/codingame/astarcraft/JVPlayer.java
@@ -0,0 +1,25 @@
+package com.codingame.astarcraft;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+public class JVPlayer {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            String line = in.next();
+        }
+        int robotCount = in.nextInt();
+        for (int i = 0; i < robotCount; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            String direction = in.next();
+        }
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+        System.out.println("4 2 U 4 3 R 2 2 D 3 3 L");
+    }
+} 
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,10 +1,11 @@
+import com.codingame.astarcraft.JVPlayer;
 import com.codingame.gameengine.runner.SoloGameRunner;
 
 public class Main {
     public static void main(String[] args) {
         SoloGameRunner gameRunner = new SoloGameRunner();
 
-        gameRunner.setAgent(RandomAI.class);
+        gameRunner.setAgent(JVPlayer.class);
         gameRunner.setTestCase("test19.json");
 
         gameRunner.start(9999);
